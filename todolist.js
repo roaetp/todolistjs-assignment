8d02ecab0d277334593a80b9a27b02ac7bf484e1
roaetp <71577129+roaetp@users.noreply.github.com>
2023-07-08 02:06:59 +0545

    Added my assignment

---
+++ todolist.js
@@ -0,0 +1,38 @@
+const inputbox = document.getElementById('input-box')
+const listcontainer = document.getElementById('list-container')
+
+
+const addTask = () => {
+    if (inputbox.value === '') {
+        alert('You must write something')
+    }
+    else {
+        let li = document.createElement('li')
+        li.innerHTML = inputbox.value
+        listcontainer.appendChild(li)
+        let span = document.createElement('span')
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    inputbox.value = ''
+}
+
+
+listcontainer.addEventListener('click', (e) => {
+    if (e.target.tagName == 'LI') {
+        e.target.classList.toggle('checked')
+        saveData()
+    }
+    else if (e.target.tagName == 'span') {
+        e.target.parentElement.remove()
+        saveData()
+    }
+}, false)
+
+function saveData() {
+    localStorage.setItem('data', listcontainer.innerHTML)
+}
+function showList() {
+    listcontainer.innerHTML = localStorage.getItem('data')
+}
+showList(); 
